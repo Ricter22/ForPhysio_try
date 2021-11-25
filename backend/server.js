f93c7da825e473ac1acc500eb54c3b2cccee752d
ric <s1094610@studenti.univpm.it>
2021-11-25 21:35:31 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const moment = require('moment');
+const socketio = require('socket.io');
+const mongoose = require('mongoose');
+
+//database connection
+mongoose.connect('mongodb+srv://ric:mobilecomputing2122@cluster0.finip.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+}).then(()=>{
+    console.log('Database connected...');
+}).catch(err=>{
+    console.log(err);
+    })
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket =>{
+    console.log('socket connected');
+})
+
+const PORT = 3000 || process.env.PORT;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
