b9dc29714fb516a1e11f2a9c4853402bab83895c
ric <s1094610@studenti.univpm.it>
2021-12-15 11:22:36 +0100

    updating the new exercises

--- backend/routes/excerciseRoutes.js
+++ backend/routes/excerciseRoutes.js
@@ -5,9 +5,17 @@ const excerciseDb = require('../models/excerciseModel');
 
 router.post('/addExcercise', (req, res)=>{
     const excercise = req.body;
-    const newExcercise = new excerciseDb({name:excercise.name, description:excercise.description, timesPerWeek:excercise.timesPerWeek});
-    newExcercise.save();
-    res.status(200).send({message: 'excercise saved'});
+
+    excerciseDb.find({name:excercise.name, description:excercise.description}, function(err, result){
+        if (result.length >= 1){
+            return res.status(422).send({message: 'excercise already saved'});
+        }
+        else{
+            const newExcercise = new excerciseDb({name:excercise.name, description:excercise.description, timesPerWeek:excercise.timesPerWeek});
+            newExcercise.save();
+            res.status(200).send({message: 'excercise saved'});
+        }
+    })
 })
 
 router.post('/excercises', (req, res)=>{
