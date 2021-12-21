53086437ed30022eb41cff2a4ff20d2134b212d6
Juan Emilio Ordóñez Márquez <80113905+juaordmar@users.noreply.github.com>
2021-12-21 12:58:00 +0100

    sign out button

--- mobile/Screens/homeScreen.js
+++ mobile/Screens/homeScreen.js
@@ -1,48 +1,55 @@
-import React, {useContext} from 'react';
-import { StyleSheet, Text, View, Button, ScrollView, RefreshControl } from 'react-native';
-import {UserContext} from '../Components/UserContext'
-import uuid from 'react-native-uuid';
+import React, { useContext } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  RefreshControl,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
+import { UserContext } from "../Components/UserContext";
+import uuid from "react-native-uuid";
 
 class HomeScreen extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-     user: '',
-     exercisesList: [],
-     refreshing: false
+      user: "",
+      exercisesList: [],
+      refreshing: false,
     };
   }
 
-  getExercises(){
-    let {value, setValue} = this.context;
+  getExercises() {
+    let { value, setValue } = this.context;
     this.setState({ user: value.username });
 
-    fetch('http://192.168.178.92:3000/excercises', {//192.168.178.92
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name: value.username
-    }),
-  }) //here we handle the status response of the server
-    .then(r =>  r.json().then(data => ({status: r.status, body: data})))
-    .then(obj => {
-      
-      if (obj.status == 200) {
-        this.setState({ exercisesList: obj.body });
-        console.log(this.state.exercisesList);
-      }
-      
-    });
+    fetch("http://192.168.178.92:3000/excercises", {
+      //192.168.178.92
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: value.username,
+      }),
+    }) //here we handle the status response of the server
+      .then((r) => r.json().then((data) => ({ status: r.status, body: data })))
+      .then((obj) => {
+        if (obj.status == 200) {
+          this.setState({ exercisesList: obj.body });
+          console.log(this.state.exercisesList);
+        }
+      });
   }
 
-  onRefresh(){
-    this.setState({refreshing: true});
+  onRefresh() {
+    this.setState({ refreshing: true });
     this.getExercises();
-    this.setState({refreshing: false});
-
+    this.setState({ refreshing: false });
   }
 
   componentDidMount() {
@@ -50,28 +57,46 @@ class HomeScreen extends React.Component {
   }
 
   render() {
-
-    const excercises = this.state.exercisesList.map(excercise => (
-      <View key={uuid.v4()} >
-          <Text >{excercise.description}</Text>
-          <Text >{excercise.timesPerWeek}</Text>
-        </View>
-      
+    const excercises = this.state.exercisesList.map((excercise) => (
+      <View key={uuid.v4()}>
+        <Text>{excercise.description}</Text>
+        <Text>{excercise.timesPerWeek}</Text>
+      </View>
     ));
 
     return (
       <View style={styles.container}>
         <ScrollView
           refreshControl={
-            <RefreshControl 
-            refreshing = {this.state.refreshing}
-            onRefresh = {this.onRefresh.bind(this)}
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this.onRefresh.bind(this)}
             />
           }
         >
           <Text>{this.state.user}</Text>
           {excercises}
         </ScrollView>
+        <TouchableOpacity
+          style={{
+            top: -15,
+            left: 140,
+          }}
+          onPress={() =>
+            Alert.alert("Exit", "Do you want to sign out?", [
+              {
+                text: "Yes",
+                onPress: () => this.props.navigation.navigate("Login"),
+              },
+              { text: "No" },
+            ])
+          }
+        >
+          <Image
+            style={{ width: 75, height: 75 }}
+            source={require("mobile/images/lightlogo_preview_rev_1.png")}
+          />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -79,13 +104,13 @@ class HomeScreen extends React.Component {
 HomeScreen.contextType = UserContext;
 // ...
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default HomeScreen;
+
